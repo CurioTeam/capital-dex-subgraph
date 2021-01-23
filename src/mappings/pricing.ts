b64202192d120a1584b418d3dc270b24837359f7
Dmitry Ivonin <dmitr-ivon@ya.ru>
2021-01-23 21:59:19 +0300

    chore: fork subgraph

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -40,22 +40,15 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+  '0xF56b164efd3CFc02BA739b719B6526A6FA1cA32a', // CGT
+  '0x13339fD07934CD674269726EdF3B5ccEE9DD93de', // CUR
   '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
-  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8' // yCurv
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+  '0x2f4d4cFAb714e4573189B300293944673Fe0efF7', // CGT
+  '0x558FC7FA5471Ff77c56b9cB37207d099EAcE8379', // CSC
+  '0x42Bbfc77Ee4Ed0efC608634859a672D0cf49e1b4', // CUR
+  '0x330294de10bAd15f373BA7429Ab9685eDe43c13f', // DAI
+  '0xd0A1E359811322d97991E03f863a0C30C2cF029C', // WETH
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
