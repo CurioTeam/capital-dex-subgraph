970e94fcdb64815537393c0b7cd05f691925c5bb
Dmitry Ivonin <dmitr-ivon@ya.ru>
2022-03-27 19:10:45 +0300

    feat: reduce the minimum liquidity threshold to zero

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -54,11 +54,8 @@ let WHITELIST: string[] = [
   '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
 ]
 
-// minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('400000')
-
 // minimum liquidity for price to get tracked
-let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2')
+let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('0')
 
 /**
  * Search through graph to find derived Eth per token.
